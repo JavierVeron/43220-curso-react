018fef85c68d17ebafed561bfad7979df2ad40c2
Javier Veron <javier.veron@gmail.com>
2023-05-02 20:48:41 -0300

    Clase 3

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,114 @@
+import React from "react";
+
 const Main = () => {
+    //Sugar Sintax
+    /* let i = 1;
+    i+= 1;
+    console.log(i);
+    i++;
+    console.log(i); */
+
+    // Código original
+    /* const condition = true;
+    let result = null
+    
+    if (condition) {
+        result = "Correcto!";
+    } else {
+        result = "Incorrecto!";
+    }
+
+    console.log(`El resultado es ${result}`); */
+
+    // Código utilizando sugar syntax
+    /* const condition = false;
+    console.log(`El resultado es: ${condition ? "Correcto!" : "Incorrecto!"}`); */
+
+    // Operador Spread
+    // Operador Spread para desparramr objetos de un Array
+    /* const productos = [
+        {id:1, nombre:"Coca Cola", precio:700},
+        {id:2, nombre:"Pepsi", precio:650}
+    ];
+    const productosActualizados = [...productos, {id:3, nombre:"Manaos", precio:500}];
+    console.log(productosActualizados); */
+
+    // Operador Spread para desparramar las propiedades de un Objeto a un nuevo Objeto
+    /* const persona = {dni:1112233, nombre:"Juan Perez"};
+    const nuevaPersona = {...persona, nombre:"John Perez", sexo:"Masculino", redesSociales:["IG", "FB", "TW"]};
+    console.log(nuevaPersona); */
+
+    // Método Find
+    /* const productos = [
+        {id:1, nombre:"Coca Cola", precio:700},
+        {id:2, nombre:"Pepsi", precio:650},
+        {id:3, nombre:"Manaos", precio:500}
+    ]; */
+    //let producto = productos.find(item => item.id === 2);
+    //console.log(producto);
+    /* let valorBuscado = 3;
+
+    for (let i=0; i<productos.length; i++) {
+        if (productos[i].id === valorBuscado) {
+            console.log(productos[i]);
+            break;
+        }
+    }
+
+    for (let producto of productos) {
+        if (producto.id === valorBuscado) {
+            console.log(producto);
+            break;
+        }
+    } */
+
+
+    // JSX
+    // Representación de un elemento HTML
+    //{React.createElement("div", {className:"textoDestacado"}, "Hola Coders!")}
+
+    let estilos = {
+        color:"white",
+        backgroundColor:"#336699",
+        padding:"5px"
+    }
+
+    const codigo = <div style={estilos}>Hola a Todos #3</div>
+
+    const saludar = () => {
+        alert("Hola a Todos!");
+    }
+
+    let modoNoche = {
+        color:"white",
+        backgroundColor:"black"
+    };
+
+    let modoDia = {
+        color:"black",
+        backgroundColor:"white"
+    };
+
+    let esDeDia = false;
+    let valor = 100;
+
     return (
-        <div className="container-fluid">
+        <div className="container-fluid py-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src={"https://img.asmedia.epimg.net/resizer/dOqXPoqZUAzxA3YGvwUd43BzI9Y=/1472x828/filters:focal(2165x1125:2175x1135)/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/2LVRGJQLT27CD4MEE56YFTZIZY.jpg"} alt={"Lionel Messi #1"} className="d-block w-100" />
-                            </div>
-                            <div className="carousel-item">
-                                <img src={"https://www.lavoz.com.ar/resizer/YvO8Q1P2V2WXyWxIABNPUzl039s=/980x640/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/DYYSZ2IZCRCHHLH6RDSEXCS4EI.jpg"} alt={"Lionel Messi #2"} className="d-block w-100" />
-                            </div>
-                        </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
+                    <h1>Clase 3</h1>
+                    {React.createElement("div", {className:"textoDestacado"}, "Hola Coders!")}
+                    <div style={estilos}>Hola a Todos #2</div>
+                    {codigo}
+                    <div style={{color:"white", backgroundColor:"#6699CC", padding:"5px"}}>Hola a Todos #4</div>
+                    <div style={{color:"white", backgroundColor:"#6699CC", padding:"5px"}}>Hola a Todos #4</div>
+                    <div style={esDeDia ? modoDia : modoNoche}>¿Es de Día?</div>
+                    <div style={estilos}><p>El Valor es: {valor * 2}</p></div>
+                    <div>
+                        <img src={"https://www.pequerecetas.com/wp-content/uploads/2013/07/hamburguesas-caseras-receta.jpg"} alt={"Hamburguesa Casera"} width={180} />
+                    </div>
+                    <div>
+                        <input type="button" name="nombre" className="btn btn-warning" value="Enviar" onClick={saludar} />
                     </div>
                 </div>
             </div>
